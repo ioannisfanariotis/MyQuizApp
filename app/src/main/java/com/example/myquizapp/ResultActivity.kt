373cbdadfee4d405a856da5185bdd9d7ea8b3ac2
ioannisfanariotis <109162046+ioannisfanariotis@users.noreply.github.com>
2022-10-04 22:21:49 +0300

    Update

--- app/src/main/java/com/example/myquizapp/ResultActivity.kt
+++ app/src/main/java/com/example/myquizapp/ResultActivity.kt
@@ -1,10 +1,10 @@
 package com.example.myquizapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
 
@@ -12,17 +12,18 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        var username: TextView = findViewById(R.id.usermame)
-        var score: TextView = findViewById(R.id.score)
-        var finish: Button = findViewById(R.id.btnfinish)
+        val username: TextView = findViewById(R.id.usermame)
+        val score: TextView = findViewById(R.id.score)
+        val finish: Button = findViewById(R.id.btnfinish)
 
         username.text = intent.getStringExtra(Constants.USER_NAME)
-        var totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
-        var correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
 
         score.text = "Your score is $correctAnswers out of $totalQuestions!"
 
-        finish.setOnClickListener{
-            startActivity(Intent(this, MainActivity::class.java))}
+        finish.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
